f72906f0f99598bb065d1fcc39ef998a57c826f9
Maple <wjxa20152015@gmail.com>
2025-04-22 15:45:35 +0800

    fix error

--- fastqc_py/main.py
+++ fastqc_py/main.py
@@ -7,7 +7,8 @@ def main():
     argv = sys.argv[1:]
     fastqc_path = Path(__file__).resolve().parent.parent.joinpath("fastqc_lib")
     fastqc_path_str = str(fastqc_path).replace("\\", "/")
-    classpath = ".;./sam-1.103.jar;./jbzip2-0.9.jar".replace(".", fastqc_path_str)
+    classpath = "!;!/sam-1.103.jar;!/jbzip2-0.9.jar".replace("!", fastqc_path_str)
+    print(classpath)
     subprocess.Popen(
         f"java -Xmx250m -classpath {classpath} uk.ac.babraham.FastQC.FastQCApplication "
         + " ".join(argv),
